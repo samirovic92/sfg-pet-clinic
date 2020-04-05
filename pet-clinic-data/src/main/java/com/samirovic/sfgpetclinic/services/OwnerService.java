b83a91b50ac02bf2a942e8f3b19c170bc70cb3b4
samir <s.elidrissi1992@gmail.com>
2020-04-05 22:04:44 +0200

    Add Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/samirovic/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.samirovic.sfgpetclinic.services;
+
+import com.samirovic.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/samirovic/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.samirovic.sfgpetclinic.services;
+
+import com.samirovic.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
