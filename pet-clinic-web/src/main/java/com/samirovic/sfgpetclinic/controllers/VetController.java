cc7149487826eefe782db7a949396dbaf07c7c18
samir <s.elidrissi1992@gmail.com>
2020-04-08 01:12:04 +0200

    Add Vet Clinic index Page and Controller. Closes #7.

---
+++ pet-clinic-web/src/main/java/com/samirovic/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.samirovic.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets","/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
